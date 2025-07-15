bd709fb44cc5c72c9efb4c38897ee4fc4e6bd21d
marko.sladojevic <mrsladoje@gmail.com>
2025-07-15 13:28:18 +0200

    Added City class in models folder

---
+++ models/city.py
@@ -0,0 +1,14 @@
+import math 
+
+class City:
+    def __init__(self, id, x, y):
+        self.id = id
+        self.x = x
+        self.y = y
+    def distance_to(self, other_city):
+        return math.sqrt((self.x-other_city.x)**2 + (self.y-other_city.y)**2)
+    
+    def __str__(self):
+        return f"{self.id} - ({self.x},{self.y})"
+
+        
