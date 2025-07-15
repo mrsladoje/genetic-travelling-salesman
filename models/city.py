ef5bad21cad7e3bb7c442baf1ca53314b0af75d6
marko.sladojevic <mrsladoje@gmail.com>
2025-07-15 15:35:27 +0200

    Added caching to distance calculation

--- models/city.py
+++ models/city.py
@@ -1,4 +1,5 @@
-import math 
+import math
+from functools import lru_cache
 
 class City:
     def __init__(self, id, x, y):
@@ -7,9 +8,16 @@ class City:
         self.y = y
         
     def distance_to(self, other_city):
-        return math.sqrt((self.x-other_city.x)**2 + (self.y-other_city.y)**2)
+        return self._calculate_distance(self.id, self.x, self.y, 
+                                      other_city.id, other_city.x, other_city.y)
+    
+    @staticmethod
+    @lru_cache(maxsize=None)  
+    def _calculate_distance(id1, x1, y1, id2, x2, y2):
+        if id1 > id2:
+            id1, x1, y1, id2, x2, y2 = id2, x2, y2, id1, x1, y1
+        
+        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
     
     def __str__(self):
         return f"{self.id} - ({self.x},{self.y})"
-
-        
